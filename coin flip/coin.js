494d8341ae28cd8b6672fda55a77d90ee6d5e360
KINYENJE <njengajames232@gmail.com>
2023-05-31 12:26:37 +0300

    ?

--- coin flip/coin.js
+++ coin flip/coin.js
@@ -19,7 +19,7 @@ http.createServer((req,res) => {
         const result = toss()
         console.log(result)
         res.writeHead(200,{'Content-Type':'text/plain'})
-        res.end(result)
+        res.end()
     } else {
         res.writeHead(404,{'Content-Type':'text/html'})
         res.end("404 Not Found")
