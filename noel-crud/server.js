07b7255fefbeeb30f8fad836829532b73008caf6
KINYENJE <njengajames232@gmail.com>
2023-07-03 23:15:26 +0300

    case sensitivity

--- noel-crud/server.js
+++ noel-crud/server.js
@@ -23,11 +23,11 @@ const rappers = {
     occupation: 'rapper',
     netWorth: 1500000    
     },
-    'Kinyenje' : {
+    'kinyenje' : {
         name: 'Kinyenje',
         age: 23,
         occupation: 'software developer',
-        netWorth: 1500000000000
+        netWorth: 1500000000000 
     }
 }
 
@@ -35,11 +35,16 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api', (req, res) => {
-    res.json(rappers)
+app.get('/api/:name', (req, res) => {
+    const rapperName = req.params.name.toLowerCase()
+    if (rappers[rapperName]) /* we use the square brackets instead of dot because of the spaces in the rapper names */ 
+    {
+        res.json(rappers[rapperName])
+    }else
+        res.json(rappers.kinyenje) /** kinyenje is one word hence can be refered by use of dot notation  */
 })
 
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on port ${PORT}`) 
 })
